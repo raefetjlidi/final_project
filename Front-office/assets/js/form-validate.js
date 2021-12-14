7c20eb38a88b49d696a9b57f1db6ddc0b2e21b4e
dato skander <raefet.jlidi@esprit.tn>
2021-12-15 00:19:07 +0100

    fix some bugs

--- Front-office/assets/js/form-validate.js
+++ Front-office/assets/js/form-validate.js
@@ -1,38 +1,22 @@
-function valid_datas( f ){
+function valid_datas(f){
 	
 	if( f.name.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your name must not be empty!</span>');
-		notice( f.name );
+		jQuery('#form_status').html('<span class="wrong">Taper votre nom!</span>');
+		return notice( f.name );
 	}else if( f.email.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your email must not be empty and correct format!</span>');
-		notice( f.email );
+		jQuery('#form_status').html('<span class="wrong">Taper un email!</span>');
+		return notice( f.email );
 	//}else if( f.phone.value == '' ){
 		//jQuery('#form_status').html('<span class="wrong">Your phone must not be empty and correct format!</span>');
 		//notice( f.phone );
 	}else if( f.subject.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your subject must not be empty!</span>');
-		notice( f.subject );
-	}else if( f.message.value == '' ){
-		jQuery('#form_status').html('<span class="wrong">Your message must not be empty!</span>');
-		notice( f.message );
-	}else{
-		 jQuery.ajax({
-			url: 'mail.php',
-			type: 'post',
-			data: jQuery('form#fruitkha-contact').serialize(),
-			complete: function(data) {
-				jQuery('#form_status').html(data.responseText);
-				jQuery('#fruitkha-contact').find('input,textarea').attr({value:''});
-				jQuery('#fruitkha-contact').css({opacity:1});
-				jQuery('#fruitkha-contact').remove();
-			}
-		});
-		jQuery('#form_status').html('<span class="loading">Sending your message...</span>');
-		jQuery('#fruitkha-contact').animate({opacity:0.3});
-		jQuery('#fruitkha-contact').find('input,textarea,button').css('border','none').attr({'disabled':''});
+		jQuery('#form_status').html('<span class="wrong">Taper un sujet!</span>');
+		return notice( f.subject );
+	}else if( f.explication.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Taper un message!</span>');
+		notice( f.explication );
 	}
-	
-	return false;
+	return   true;
 }
 
 function notice( f ){
